bea285617063a2b336bae18f1ea31fa20f3c59fb
Kalinichenko Vitaliy <kalinichenkovv@nextmail.ru>
2022-08-30 18:51:26 +0300

    remake calendar

--- public/calendar/pppr.php
+++ public/calendar/pppr.php
@@ -3,11 +3,11 @@
 $month = $_GET['month'];
 $year = $_GET['year'];
 $today = $_GET['today'];
-$trp = $_GET['trp'];
+$dt = $_GET['dt'];
 $header = $_GET['header'];
 $lives = $_GET['lives'];
+$trp = $_GET['trp'];
 $scripture = $_GET['scripture'];
-$dt = $_GET['dt'];
 
 $contents = file_get_contents("http://www.holytrinityorthodox.com/ru/calendar/calendar.php?month=$month&today=$today&year=$year&dt=$dt&header=$header&lives=$lives&trp=$trp&scripture=$scripture");
 
--- src/components/asside/IconDay/IconDay.tsx
+++ src/components/asside/IconDay/IconDay.tsx
@@ -3,139 +3,111 @@ import React from 'react';
 import styles from './iconDay.module.scss';
 
 import './calendar_style_blue.css';
-import { loadCalendar2r } from '../../../utils/apiCalendar';
+// import { loadCalendar2r } from '../../../utils/apiCalendar';
 
 import { EColor, Text } from '../../../shared/Text';
 
 // Скрипт загрузки календаря
-// const createLoadCalendar = () => {
-//   const today = new Date(); // Определить текущую дату
-//   let mm = today.getMonth() + 1; // Определить текущий месяц
-//   let dd = today.getDate(); // Определить текущий день
-//   let yy = today.getFullYear(); // Определить текущий год
-//   let dt = 1; // Выводить дату в формате "григорианский/юлианский"
-//   let hh = 1; // Выводить заголовок
-//   let ll = 1; // Выводить жития святых
-//   let tt = 1; // Выводить тропари
-//   let ss = 1; // Выводить Евангельские чтения
+const createLoadCalendar = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let xmlHttp;
 
-//   let xmlHttp;
+  const scriptLoadCalendar = document.createElement('script');
+  scriptLoadCalendar.id = 'loadCalendar';
+  scriptLoadCalendar.type = 'text/javascript';
+  scriptLoadCalendar.innerHTML = `
+    function stateChanged2r() {
+      if (xmlHttp.readyState === 4) {
+        // 4 = "loaded"
+        if (xmlHttp.status === 200) {
+          // 200 = "OK"
+          document.getElementById('T1R').innerHTML = xmlHttp.responseText;
+        } else {
+          alert('Problem retrieving data: ' + xmlHttp.statusText);
+        }
+      }
+    }
 
-//   const scriptLoadCalendar = document.createElement('script');
-//   scriptLoadCalendar.id = 'loadCalendar';
-//   scriptLoadCalendar.type = 'text/javascript';
-//   scriptLoadCalendar.innerHTML = `
-//     function stateChanged2r() {
-//       if (xmlHttp.readyState === 4) {
-//         // 4 = "loaded"
-//         if (xmlHttp.status === 200) {
-//           // 200 = "OK"
-//           document.getElementById('T1R').innerHTML = xmlHttp.responseText;
-//         } else {
-//           alert('Problem retrieving data: ' + xmlHttp.statusText);
-//         }
-//       }
-//     }
+    function loadCalendar2r(mm, dd, yy, dt, hh, ll, tt, ss) {
+      xmlHttp = null;
+      if (window.XMLHttpRequest) {
+        // code for Firefox, Opera, IE7, etc.
+        xmlHttp = new XMLHttpRequest();
+      } else if (window.ActiveXObject) {
+        // code for IE6, IE5
+        xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+      }
 
-//     function loadCalendar2r(mm, dd, yy, dt, hh, ll, tt, ss) {
-//       xmlHttp = null;
-//       if (window.XMLHttpRequest) {
-//         // code for Firefox, Opera, IE7, etc.
-//         xmlHttp = new XMLHttpRequest();
-//       } else if (window.ActiveXObject) {
-//         // code for IE6, IE5
-//         xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
-//       }
+      if (xmlHttp != null) {
+        let par = 'calendar/pppr.php';
+        par =
+          par +
+          '?month=' +
+          mm +
+          '&today=' +
+          dd +
+          '&year=' +
+          yy +
+          '&dt=' +
+          dt +
+          '&header=' +
+          hh +
+          '&lives=' +
+          ll +
+          '&trp=' +
+          tt +
+          '&scripture=' +
+          ss;
+        par = par + '&sid=' + Math.random();
 
-//       if (xmlHttp != null) {
-//         let par = 'calendar/pppr.php';
-//         par =
-//           par +
-//           '?month=' +
-//           mm +
-//           '&today=' +
-//           dd +
-//           '&year=' +
-//           yy +
-//           '&dt=' +
-//           dt +
-//           '&header=' +
-//           hh +
-//           '&lives=' +
-//           ll +
-//           '&trp=' +
-//           tt +
-//           '&scripture=' +
-//           ss;
-//         par = par + '&sid=' + Math.random();
+        xmlHttp.onreadystatechange = stateChanged2r;
 
-//         xmlHttp.onreadystatechange = stateChanged2r;
-
-//         xmlHttp.open('GET', par, true);
-//         xmlHttp.send(null);
-//       } else {
-//         alert('Your browser does not support XMLHTTP.');
-//       }
-//     }
-
-//     loadCalendar2r(${mm}, ${dd}, ${yy}, ${dt}, ${hh}, ${ll}, ${tt}, ${ss});
-//   `;
-//   document.head.append(scriptLoadCalendar);
-// };
+        xmlHttp.open('GET', par, true);
+        xmlHttp.send(null);
+      } else {
+        alert('Your browser does not support XMLHTTP.');
+      }
+    }
+  `;
+  document.head.append(scriptLoadCalendar);
+};
 
 // Скрипт вызова 'loadCalendar' в элементе DOM id='T1R'
-// const showCalendar = () => {
-//   const today = new Date(); // Определить текущую дату
-//   let mm = today.getMonth() + 1; // Определить текущий месяц
-//   let dd = today.getDate(); // Определить текущий день
-//   let yy = today.getFullYear(); // Определить текущий год
-//   let dt = 1; // Выводить дату в формате "григорианский/юлианский"
-//   let hh = 1; // Выводить заголовок
-//   let ll = 1; // Выводить жития святых
-//   let tt = 1; // Выводить тропари
-//   let ss = 1; // Выводить Евангельские чтения
+const showCalendar = () => {
+  const today = new Date(); // Определить текущую дату
+  let mm = today.getMonth() + 1; // Определить текущий месяц
+  let dd = today.getDate(); // Определить текущий день
+  let yy = today.getFullYear(); // Определить текущий год
+  let dt = 1; // Выводить дату в формате "григорианский/юлианский"
+  let hh = 1; // Выводить заголовок
+  let ll = 1; // Выводить жития святых
+  let tt = 1; // Выводить тропари
+  let ss = 1; // Выводить Евангельские чтения
 
-//   const scriptCalendar = document.createElement('script');
-//   scriptCalendar.id = 'showCalendar';
-//   scriptCalendar.type = 'text/javascript';
-//   scriptCalendar.innerHTML = `
-//     loadCalendar2r(${mm}, ${dd}, ${yy}, ${dt}, ${hh}, ${ll}, ${tt}, ${ss});
-//   `;
-//   document.head.append(scriptCalendar);
-// };
+  const scriptCalendar = document.createElement('script');
+  scriptCalendar.id = 'showCalendar';
+  scriptCalendar.type = 'text/javascript';
+  scriptCalendar.innerHTML = `
+    loadCalendar2r(${mm}, ${dd}, ${yy}, ${dt}, ${hh}, ${ll}, ${tt}, ${ss});
+  `;
+  document.head.append(scriptCalendar);
+};
 
 export function IconDay() {
-  // const [isCreateLoadCalendar, setIsCreateLoadCalendar] = React.useState(false);
-  // const [isShowCalendar, setIsShowCalendar] = React.useState(false);
+  const [isCreateLoadCalendar, setIsCreateLoadCalendar] = React.useState(false);
+  const [isShowCalendar, setIsShowCalendar] = React.useState(false);
 
   React.useEffect(() => {
-    const today = new Date(); // Определить текущую дату
-    let mm = today.getMonth() + 1; // Определить текущий месяц
-    let dd = today.getDate(); // Определить текущий день
-    let yy = today.getFullYear(); // Определить текущий год
-    let dt = 1; // Выводить дату в формате "григорианский/юлианский"
-    let hh = 1; // Выводить заголовок
-    let ll = 1; // Выводить жития святых
-    let tt = 1; // Выводить тропари
-    let ss = 1; // Выводить Евангельские чтения
+    if (!isCreateLoadCalendar) {
+      if (!document.getElementById('loadCalendar')) createLoadCalendar();
+      if (!isCreateLoadCalendar) setIsCreateLoadCalendar(true);
+    }
 
-    loadCalendar2r(mm, dd, yy, dt, hh, ll, tt, ss);
-    // if (!isCreateLoadCalendar) {
-
-    // if (!document.getElementById('loadCalendar')) loadCalendar2r(mm,dd,yy,dt,hh,ll,tt,ss);
-    // if (!isCreateLoadCalendar) setIsCreateLoadCalendar(true);
-    // }
-
-    // if (isCreateLoadCalendar && !isShowCalendar) {
-    //   if (!document.getElementById('showCalendar')) showCalendar();
-    //   if (!isShowCalendar) setIsShowCalendar(true);
-    // }
-
-    // return () => {
-    //   document.getElementById('showCalendar')?.remove();
-    //   document.getElementById('loadCalendar')?.remove();
-    // };
-  }, []);
+    if (isCreateLoadCalendar && !isShowCalendar) {
+      if (!document.getElementById('showCalendar')) showCalendar();
+      if (!isShowCalendar) setIsShowCalendar(true);
+    }
+  }, [isCreateLoadCalendar, isShowCalendar]);
 
   return (
     <div className={styles.iconDay}>
